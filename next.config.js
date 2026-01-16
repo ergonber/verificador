404868a6679c6249f7e7c8fb3ec0f6f1b26d4606
ergonber <ernesto.gonzales.bermudez@gmail.com>
2026-01-16 09:19:31 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Agrega estas configuraciones
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 }
 
 module.exports = nextConfig
