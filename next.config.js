3473387e4324ff758fa8ffc4aeb4e7cb910f3a9d
ergonber <ernesto.gonzales.bermudez@gmail.com>
2026-02-05 11:28:28 -0400

    Enable SWC minification and add security headers

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Opcional: Configurar headers de seguridad
+  swcMinify: true,
+  // Configurar headers de seguridad
   async headers() {
     return [
       {
@@ -11,10 +12,18 @@ const nextConfig = {
             key: 'X-Content-Type-Options',
             value: 'nosniff',
           },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
         ],
       },
     ];
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
